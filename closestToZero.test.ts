48a89ebfbb71885f3791e479d37489643851c33e
AlexCham <chamati.alexandre@gmail.com>
2020-03-02 23:26:18 +0100

    declaration test modified

--- closestToZero.test.ts
+++ closestToZero.test.ts
@@ -30,18 +30,18 @@ test("return 2 with [-5, 4, 2, 3, -2]", () => {
 
 // Provided tests
 
-test("8, 5, 10", () => {
+test("return 5 with [8, 5, 10]", () => {
 	expect(closestToZero([8, 5, 10])).toBe(5);
 });
 
-test("5, 4, -9, 6, 10, -1, 8", () => {
+test("return -1 with [5, 4, -9, 6, 10, -1, 8]", () => {
 	expect(closestToZero([5, 4, -9, 6, 10, -1, 8])).toBe(-1);
 });
 
-test("8, 2, 3, -2", () => {
+test("return 2 with [8, 2, 3, -2]", () => {
 	expect(closestToZero([8, 2, 3, -2])).toBe(2);
 });
 
-test("2, 0", () => {
+test("return 0 with [2, 0]", () => {
 	expect(closestToZero([2, 0])).toBe(0);
 });
